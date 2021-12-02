e615c4a92b5b978ca6d3a11704577c694d68d156
Colette <colette.rs.shaw@gmail.com>
2021-12-02 21:16:01 +0000

    Update

--- day1Part1.js
+++ day1Part1.js
@@ -9,29 +9,24 @@
 // //     .then(text => console.log(text));
 // // }
 //
-// // function readInput() {
-// //   FileReader.read
-// // }
-//
-// function run() {
-//   readInput();
-// }
-//
-// run();
 
-const fs = require('fs');
+function readInput() {
+  const fs = require('fs');
 
-try {
-    // read contents of the file
+  try {
     const data = fs.readFileSync('day1Input.txt', 'UTF-8');
-
-    // split the contents by new line
     const lines = data.split(/\r?\n/);
 
-    // print all lines
     lines.forEach((line) => {
-        console.log(line);
+      console.log(line);
     });
-} catch (err) {
+  } catch (err) {
     console.error(err);
+  }
 }
+
+function run() {
+  readInput();
+}
+
+run();
