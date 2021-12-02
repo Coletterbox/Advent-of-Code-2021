b124607351d12354f924f4b89259fe071f6860d1
Colette <colette.rs.shaw@gmail.com>
2021-12-02 21:28:32 +0000

    Finish?

--- day1Part1.js
+++ day1Part1.js
@@ -1,32 +1,37 @@
-// // // npm install node-fetch
-// //
-// import fetch from 'node-fetch';
-// // const fetch = require('node-fetch');
-//
-// // function readInput() {
-// //   fetch('day1Input1.txt')
-// //     .then(response => response.text())
-// //     .then(text => console.log(text));
-// // }
-//
-
-function readInput() {
+function readInput(fileName) {
   const fs = require('fs');
+  const inputArray = [];
 
   try {
-    const data = fs.readFileSync('day1Input.txt', 'UTF-8');
+    const data = fs.readFileSync(fileName, 'UTF-8');
     const lines = data.split(/\r?\n/);
 
     lines.forEach((line) => {
-      console.log(line);
+      // console.log(line);
+      inputArray.push(line);
     });
   } catch (err) {
     console.error(err);
   }
+
+  return inputArray;
+}
+
+function doThing(fileName) {
+  const inputArray = readInput(fileName);
+  let index = 0;
+
+  for (let i = 1; i < inputArray.length; i++) {
+    if (inputArray[i] > inputArray[i - 1]) {
+      index++;
+    }
+  }
+  console.log(index);
+  return index;
 }
 
 function run() {
-  readInput();
+  doThing('day1Input.txt');
 }
 
 run();
