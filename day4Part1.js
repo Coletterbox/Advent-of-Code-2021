dadd1924b7504e27b5ccee3f14d97c469298bd0d
Colette <colette.rs.shaw@gmail.com>
2021-12-11 02:19:44 +0000

    Return sum of unmarked numbers

--- day4Part1.js
+++ day4Part1.js
@@ -7,20 +7,20 @@ function readInput(fileName) {
     const lines = data.split(/\r?\n/);
 
     lines.forEach((line) => {
-      console.log(line);
+      // console.log(line);
       inputArray.push(line);
     });
   } catch (err) {
     console.error(err);
   }
 
-  console.log(inputArray);
+  // console.log(inputArray);
   return inputArray;
 }
 
 function getNumbers(fileName) {
   let inputArray = readInput(fileName);
-  console.log('numbers:', inputArray[0]);
+  // console.log('numbers:', inputArray[0]);
   return inputArray[0];
 }
 
@@ -40,17 +40,17 @@ function getBoards(fileName) {
     newBoardArray.push(boardArray.slice(i, i+6));
   }
 
-  console.log('boardArray:', boardArray);
-  console.log('newBoardArray:', newBoardArray);
+  // console.log('boardArray:', boardArray);
+  // console.log('newBoardArray:', newBoardArray);
 
   for (let i = 0; i < newBoardArray.length; i++) {
     for (let j = 0; j < newBoardArray[i].length; j++) {
-      console.log('newBoardArray[i][j]', newBoardArray[i][j]);
+      // console.log('newBoardArray[i][j]', newBoardArray[i][j]);
       newBoardArray2.push(newBoardArray[i][j].split(' ').filter(el => {return el != null && el != ''}));
     }
   }
 
-  console.log('newBoardArray2:', newBoardArray2);
+  // console.log('newBoardArray2:', newBoardArray2);
 
   let newArrayIndex = 0;
   for (let i = 0; i < newBoardArray2.length; i++) {
@@ -68,7 +68,7 @@ function getBoards(fileName) {
       // newBoardArray3[newArrayIndex]+='test';
       // newBoardArray3[newArrayIndex].push('test');
       newBoardArray3[newArrayIndex].push(newBoardArray2[i]);
-      console.log(newBoardArray3[newArrayIndex]);
+      // console.log(newBoardArray3[newArrayIndex]);
     }
   }
 
@@ -126,7 +126,12 @@ function checkNumbers(fileName) {
                   indexOfLastNumber = indexOfNumbers;
                   console.log('indexOfLastNumber', indexOfLastNumber);
                   console.log('index of winning board', current[0]);
+
                   return current[0]; // this is the index of the winning board
+
+                  // let lastNumber = numbers[indexOfLastNumber];
+                  // console.log(lastNumber);
+                  // return lastNumber;
               }
             }
             // check markerArray for five with same j and l (and consecutive ks)
@@ -135,7 +140,77 @@ function checkNumbers(fileName) {
       }
     }
   }
-  console.log(markerArray);
+
+  // these four lines should really go somewhere else
+  // console.log(markerArray);
+  // let sumOfUnmarkedNumbers = findSumOfUnmarkedNumbers(fileName);
+  // console.log(sumOfUnmarkedNumbers);
+  // getResult(sumOfUnmarkedNumbers, indexOfNumbers);
+}
+
+// this is the same thing copied and pasted but returning different things; I'm tired
+function getFinalNumber(fileName) {
+  let numbers = getNumbers(fileName);
+  numbers = numbers.split(',');
+  const boards = getBoards(fileName);
+  let markerArray = [];
+  let indexOfLastNumber = 0;
+
+  for (let indexOfNumbers = 0; indexOfNumbers < numbers.length; indexOfNumbers++) {
+    for (let j = 0; j < boards.length; j++) {
+      for (let k = 0; k < boards[j].length; k++) {
+        for (let l = 0; l < boards[j][k].length; l++) {
+          // console.log(boards[j][k][l]);
+          if (boards[j][k][l] === numbers[indexOfNumbers]) {
+            markerArray.push(j.toString() + ' ' + k.toString() + ' ' + l.toString());
+            markerArray.sort(); // this is alphabetical but it's fine for this purpose
+            // check markerArray for five with same j and k (and consecutive ls (keep in mind real input will produce two-digit numbers)
+            for (let i = 4; i < markerArray.length; i++) {
+              let previous4 = markerArray[i-4].split(' ');
+              let previous3 = markerArray[i-3].split(' ');
+              let previous2 = markerArray[i-2].split(' ');
+              let previous = markerArray[i-1].split(' ');
+              let current = markerArray[i].split(' ');
+              // console.log('previous:', previous);
+              // console.log('current:', current);
+              // let tally = [];
+              // ...idk I WAS TIRED IT'S FINE
+              if (markerArray.length > 4
+                && current[0] == previous[0]
+                && current[1] == previous[1]
+                && previous[0] == previous2[0]
+                && previous[1] == previous2[1]
+                && previous2[0] == previous3[0]
+                && previous2[1] == previous3[1]
+                && previous3[0] == previous4[0]
+                && previous3[1] == previous4[1]
+              ) {
+                  // tally.push([current[0], current[1]]);
+                  // console.log('tally:', tally
+                  // indexOfLastNumber = i; // this isn't correct; i is the index of something else
+                  indexOfLastNumber = indexOfNumbers;
+                  console.log('indexOfLastNumber', indexOfLastNumber);
+                  console.log('index of winning board', current[0]);
+
+                  // return current[0]; // this is the index of the winning board
+
+                  let lastNumber = numbers[indexOfLastNumber];
+                  console.log(lastNumber);
+                  return lastNumber;
+              }
+            }
+            // check markerArray for five with same j and l (and consecutive ks)
+          }
+        }
+      }
+    }
+  }
+
+  // these four lines should really go somewhere else
+  // console.log(markerArray);
+  // let sumOfUnmarkedNumbers = findSumOfUnmarkedNumbers(fileName);
+  // console.log(sumOfUnmarkedNumbers);
+  // getResult(sumOfUnmarkedNumbers, indexOfNumbers);
 }
 
 // 0, 1, 2, 3, 4
@@ -155,32 +230,66 @@ function checkNumbers(fileName) {
 // sort indexes
 // check list of indexes contains one of above combinations
 
+// let sumOfUnmarkedNumbers = 0;
+// function findSumOfUnmarkedNumbers(fileName) {
+//   // (of winning board)
+//   let numbers = getNumbers(fileName).split(',');
+//   numbers = numbers.slice(0, numbers.indexOf(getFinalNumber(fileName)));
+//   console.log(numbers);
+//   const boards = getBoards(fileName);
+//   const indexOfWinningBoard = checkNumbers(fileName);
+//   let winningBoard = boards[indexOfWinningBoard]; // winning board as two-dimensional array
+//   for (let i = 0; i < winningBoard.length; i++) {
+//     for (let j = 0; j < winningBoard[i].length; j++) {
+//       // for (let k = 0; k < numbers.length; k++) {
+//       //   if (winningBoard[i][j] !)
+//       // }
+//       // console.log('numbers:', numbers);
+//       // console.log('winningBoard[i][j]:', winningBoard[i][j]);
+//       // console.log(numbers.includes(winningBoard[i][j].toString()));
+//       // if (numbers.includes(winningBoard[i][j].toString()) == false) {
+//       //   sumOfUnmarkedNumbers+=parseInt(winningBoard[i][j]);
+//       //   console.log(sumOfUnmarkedNumbers);
+//       // }
+//     }
+//   }
+//   console.log(sumOfUnmarkedNumbers);
+//   return sumOfUnmarkedNumbers;
+// }
+
 let sumOfUnmarkedNumbers = 0;
 function findSumOfUnmarkedNumbers(fileName) {
   // (of winning board)
-  const numbers = getNumbers(fileName).split(',');
+  let numbers = getNumbers(fileName).split(',');
+  numbers = numbers.slice(0, numbers.indexOf(getFinalNumber(fileName))+1);
+  console.log(numbers);
   const boards = getBoards(fileName);
   const indexOfWinningBoard = checkNumbers(fileName);
   let winningBoard = boards[indexOfWinningBoard]; // winning board as two-dimensional array
   for (let i = 0; i < winningBoard.length; i++) {
     for (let j = 0; j < winningBoard[i].length; j++) {
       // for (let k = 0; k < numbers.length; k++) {
-      //   if (winningBoard[i][j] !)
-      // }
-      console.log('numbers:', numbers);
-      console.log('winningBoard[i][j]:', winningBoard[i][j]);
-      console.log(numbers.includes(winningBoard[i][j].toString()));
-      // if (numbers.includes(winningBoard[i][j].toString()) == false) {
-      //   sumOfUnmarkedNumbers+=parseInt(winningBoard[i][j]);
-      //   console.log(sumOfUnmarkedNumbers);
+  //     //   if (winningBoard[i][j] !)
+  //     // }
+  //     // console.log('numbers:', numbers);
+  //     // console.log('winningBoard[i][j]:', winningBoard[i][j]);
+  //     // console.log(numbers.includes(winningBoard[i][j].toString()));
+        if (numbers.includes(winningBoard[i][j].toString()) == false) {
+          sumOfUnmarkedNumbers+=parseInt(winningBoard[i][j]);
+          console.log(sumOfUnmarkedNumbers);
+        }
       // }
     }
   }
-  console.log(sumOfUnmarkedNumbers);
+  console.log('sumOfUnmarkedNumbers', sumOfUnmarkedNumbers);
   return sumOfUnmarkedNumbers;
 }
 
-function getResult() {
+function getResult(fileName) {
+    let sumOfUnmarkedNumbers = findSumOfUnmarkedNumbers(fileName);
+    let finalNumber = getFinalNumber(fileName);
+    console.log(sumOfUnmarkedNumbers);
+    console.log(sumOfUnmarkedNumbers * finalNumber);
     return sumOfUnmarkedNumbers * finalNumber;
 }
 
@@ -190,13 +299,15 @@ function run() {
   // const indexOfWinningBoard = checkNumbers('day4TestInput.txt');
   // console.log(boards[indexOfWinningBoard]); // winning board as two-dimensional array
 
-  // findSumOfUnmarkedNumbers('day4TestInput.txt');
+  findSumOfUnmarkedNumbers('day4TestInput.txt');
   // findSumOfUnmarkedNumbers('day4Input.txt');
 
   // checkNumbers('day4Input.txt');
-  checkNumbers('day4TestInput.txt');
+  // checkNumbers('day4TestInput.txt');
   // checkNumbers('day4TestInput2.txt');
   // checkNumbers('day4TestInput3.txt');
+
+  // getResult('day4TestInput.txt');
 }
 
 run();
