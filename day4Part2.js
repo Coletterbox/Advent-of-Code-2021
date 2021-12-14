99a4661b5b1980cdd9f329184caf9814002167d0
Colette <colette.rs.shaw@gmail.com>
2021-12-14 01:12:04 +0000

    Return properly marked board

--- day4Part2.js
+++ day4Part2.js
@@ -1,3 +1,8 @@
+// 594 * 9 = 5346
+// TODO: fix indentation
+
+const { get } = require('http');
+
 function readInput(fileName) {
     const fs = require('fs');
     const inputArray = [];
@@ -19,6 +24,63 @@ function readInput(fileName) {
   function getNumbers(fileName) {
     let inputArray = readInput(fileName);
     return inputArray[0];
+  }
+
+  function getLosingBoard(fileName) {
+    const indexOfLosingBoard = checkNumbers(fileName);
+    const boardArray = getBoards(fileName);
+    const losingBoard = boardArray[indexOfLosingBoard];
+    console.log('losingBoard:', losingBoard);
+    return losingBoard;
+  }
+
+  // because I can't be bothered to troubleshoot the existing code
+  function getAnswerToSecondPart(fileName) {
+    const losingBoard = getLosingBoard(fileName);
+    const numbers = getNumbers(fileName).split(',');
+
+    const boards = [losingBoard];
+    let markerArray = [];
+    let indexOfLastNumber = 0;
+  
+    for (let indexOfNumbers = 0; indexOfNumbers < numbers.length; indexOfNumbers++) {
+      for (let j = 0; j < boards.length; j++) {
+        for (let k = 0; k < boards[j].length; k++) {
+          for (let l = 0; l < boards[j][k].length; l++) {
+            if (boards[j][k][l] === numbers[indexOfNumbers]) {
+              markerArray.push(j.toString() + ' ' + k.toString() + ' ' + l.toString());
+              markerArray.sort(); // this is alphabetical but it's fine for this purpose
+              // check markerArray for five with same j and k (and consecutive ls (keep in mind real input will produce two-digit numbers)
+              for (let i = 4; i < markerArray.length; i++) {
+                let previous4 = markerArray[i-4].split(' ');
+                let previous3 = markerArray[i-3].split(' ');
+                let previous2 = markerArray[i-2].split(' ');
+                let previous = markerArray[i-1].split(' ');
+                let current = markerArray[i].split(' ');
+                // ...idk I WAS TIRED IT'S FINE
+                if (markerArray.length > 4
+                  && current[0] == previous[0]
+                  && current[1] == previous[1]
+                  && previous[0] == previous2[0]
+                  && previous[1] == previous2[1]
+                  && previous2[0] == previous3[0]
+                  && previous2[1] == previous3[1]
+                  && previous3[0] == previous4[0]
+                  && previous3[1] == previous4[1]
+                ) {
+                    indexOfLastNumber = indexOfNumbers;
+                    console.log('indexOfLastNumber', indexOfLastNumber);
+                    // console.log('index of winning board', current[0]); // doesn't apply anymore since it's been replaced with a single board
+                    let lastNumber = numbers[indexOfLastNumber];
+                    console.log(lastNumber);
+                    return lastNumber;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
   
   function getBoards(fileName) {
@@ -61,6 +123,65 @@ function readInput(fileName) {
     let numbers = getNumbers(fileName);
     numbers = numbers.split(',');
     const boards = getBoards(fileName);
+    // const boards = [getLosingBoard(fileName)];
+    let markerArray = [];
+    let indexOfLastNumber = 0;
+    let winningBoardIndexes = [];
+  
+    for (let indexOfNumbers = 0; indexOfNumbers < numbers.length; indexOfNumbers++) {
+      for (let j = 0; j < boards.length; j++) {
+        for (let k = 0; k < boards[j].length; k++) {
+          for (let l = 0; l < boards[j][k].length; l++) {
+            if (boards[j][k][l] === numbers[indexOfNumbers]) {
+              markerArray.push(j.toString() + ' ' + k.toString() + ' ' + l.toString());
+              markerArray.sort(); // this is alphabetical but it's fine for this purpose
+              // check markerArray for five with same j and k (and consecutive ls (keep in mind real input will produce two-digit numbers)
+              for (let i = 4; i < markerArray.length; i++) {
+                let previous4 = markerArray[i-4].split(' ');
+                let previous3 = markerArray[i-3].split(' ');
+                let previous2 = markerArray[i-2].split(' ');
+                let previous = markerArray[i-1].split(' ');
+                let current = markerArray[i].split(' ');
+                // console.log('previous:', previous);
+                // console.log('current:', current);
+                // let tally = [];
+                // ...idk I WAS TIRED IT'S FINE
+                if (markerArray.length > 4
+                  && current[0] == previous[0]
+                  && current[1] == previous[1]
+                  && previous[0] == previous2[0]
+                  && previous[1] == previous2[1]
+                  && previous2[0] == previous3[0]
+                  && previous2[1] == previous3[1]
+                  && previous3[0] == previous4[0]
+                  && previous3[1] == previous4[1]) 
+                    // idk about this indentation
+                    {
+                      indexOfLastNumber = indexOfNumbers;
+                    //   console.log('indexOfLastNumber', indexOfLastNumber);
+                    //   console.log('index of winning board', current[0]);
+  
+                      winningBoardIndexes.push(current[0]); // this is the index of the winning board
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    // console.log(winningBoardIndexes);
+    let filteredIndexes = [...new Set(winningBoardIndexes)];
+    console.log(filteredIndexes);
+    console.log('losing board:', filteredIndexes[filteredIndexes.length-1]);
+    return filteredIndexes[filteredIndexes.length-1];
+  }
+
+  // yeah, idc
+  function checkNumbers2(fileName) {
+    let numbers = getNumbers(fileName);
+    numbers = numbers.split(',');
+    // const boards = getBoards(fileName);
+    const boards = [getLosingBoard(fileName)];
     let markerArray = [];
     let indexOfLastNumber = 0;
     let winningBoardIndexes = [];
@@ -117,7 +238,8 @@ function readInput(fileName) {
   function getFinalNumber(fileName) {
     let numbers = getNumbers(fileName);
     numbers = numbers.split(',');
-    const boards = getBoards(fileName);
+    // const boards = getBoards(fileName);
+    const boards = [getLosingBoard(fileName)];
     let markerArray = [];
     let indexOfLastNumber = 0;
   
@@ -148,7 +270,7 @@ function readInput(fileName) {
                 ) {
                     indexOfLastNumber = indexOfNumbers;
                     console.log('indexOfLastNumber', indexOfLastNumber);
-                    console.log('index of winning board', current[0]);
+                    // console.log('index of winning board', current[0]); // doesn't apply anymore since it's been replaced with a single board
                     let lastNumber = numbers[indexOfLastNumber];
                     console.log(lastNumber);
                     return lastNumber;
@@ -161,6 +283,7 @@ function readInput(fileName) {
     }
   }
   
+  // I'm not cleaning these up
   let sumOfUnmarkedNumbers = 0;
   function findSumOfUnmarkedNumbers(fileName) {
     // (of winning board)
@@ -168,7 +291,7 @@ function readInput(fileName) {
     numbers = numbers.slice(0, numbers.indexOf(getFinalNumber(fileName))+1);
     console.log(numbers);
     const boards = getBoards(fileName);
-    const indexOfWinningBoard = checkNumbers(fileName);
+    const indexOfLosingBoard = checkNumbers(fileName);
     let winningBoard = boards[indexOfWinningBoard]; // winning board as two-dimensional array
     for (let i = 0; i < winningBoard.length; i++) {
       for (let j = 0; j < winningBoard[i].length; j++) {
@@ -190,12 +313,66 @@ function readInput(fileName) {
       return sumOfUnmarkedNumbers * finalNumber;
   }
   
-  function run() {
-    // getResult('day4TestInput.txt'); // 4512
-    // getResult('day4Input.txt'); // 63552
+  let sumOfUnmarkedNumbers2 = 0;
+  function findSumOfUnmarkedNumbers2(fileName) {
+    let numbers = getNumbers(fileName).split(',');
+    numbers = numbers.slice(0, numbers.indexOf(getFinalNumber(fileName))+1);
+    console.log(numbers);
+    // const boards = getBoards(fileName);
+    // const boards = getLosingBoard(fileName);
+    // const indexOfLosingBoard = checkNumbers(fileName);
+    // let winningBoard = boards[indexOfWinningBoard]; // winning board as two-dimensional array
+    let losingBoard = getLosingBoard(fileName);
+    for (let i = 0; i < losingBoard.length; i++) {
+      for (let j = 0; j < losingBoard[i].length; j++) {
+        if (numbers.includes(losingBoard[i][j].toString()) == false) {
+          sumOfUnmarkedNumbers+=parseInt(losingBoard[i][j]);
+          console.log(sumOfUnmarkedNumbers);
+        }
+      }
+    }
+    console.log('sumOfUnmarkedNumbers', sumOfUnmarkedNumbers);
+    return sumOfUnmarkedNumbers;
+  }
 
-    // checkNumbers('day4TestInput.txt');
-    checkNumbers('day4Input.txt');
+  function run() {
+    // getResult('day4TestInput.txt'); // 4512 for part 1
+    // getResult('day4Input.txt'); // 63552 for part 1
+
+    // checkNumbers('day4TestInput.txt'); // 1
+    // checkNumbers('day4Input.txt'); // 54
+
+    // getFinalNumber('day4TestInput.txt');
+
+    // findSumOfUnmarkedNumbers('day4TestInput.txt');
+    // findSumOfUnmarkedNumbers('day4Input.txt'); // 932
+
+    // getLosingBoard('day4TestInput.txt'); // this works
+    // getFinalNumber('day4TestInput.txt'); // this doesn't work
+
+    // checkNumbers2('day4TestInput.txt');
+    // findSumOfUnmarkedNumbers2('day4TestInput.txt');
+    // getFinalNumber('day4TestInput.txt');
+
+    getLosingBoard('day4Input.txt');
+    // losingBoard: [
+    //     [ '7', '83', '51', '95', '98' ],
+    //     [ '56', '93', '62', '85', '9' ],
+    //     [ '72', '14', '44', '70', '67' ],
+    //     [ '42', '4', '65', '37', '54' ],
+    //     [ '47', '82', '1', '60', '55' ]
+    //   ]
+    // losingBoard: [
+    //     [ '7', '83', '51', 000, 000 ],
+    //     [ '56', 000, 000,  000,  000 ],
+    //     [ '72', '14', '44', '70', 000 ],
+    //     [ 000, '4',  000,  '37',  000 ],
+    //     [ 000, 000, '1', '60', 000 ]
+    //   ]
+    // 85,84,30,15,46,71,64,45,13,90,63,89,62,25,87,68,73,47,65,78,2,27,67,95,88,99,96,17,42,31,91,98,57,28,38,93,43,0,55,49,22,24,82,54,59,52,3,26,9
+
+    // getAnswerToSecondPart('day4TestInput.txt');
+    // getAnswerToSecondPart('day4Input.txt');
   }
   
   run();
---
+++ day5Part1.js
@@ -0,0 +1,176 @@
+// parse input
+// find max X and Y; create 2D array for lines (populate with 0s)
+// only consider horizontal and vertical lines, i.e. Xs are equal or Ys are equal
+// increment relevant array items (plot lines)
+// count array items that are over 1
+
+function readInput(fileName) {
+    const fs = require('fs');
+    const inputArray = [];
+  
+    try {
+        const data = fs.readFileSync(fileName, 'UTF-8');
+        const lines = data.split(/\r?\n/);
+  
+        lines.forEach((line) => {
+            inputArray.push(line);
+        });
+    } catch (err) {
+        console.error(err);
+    }
+  
+    console.log(inputArray);
+    return inputArray;
+}
+
+function parseInput(fileName) {
+    const inputArray = readInput(fileName);
+    let resultArray = [];
+    // let resultArray2 = [];
+    inputArray.forEach(element => {
+        resultArray.push(element.split(' -> '));
+    });
+
+    console.log('resultArray:', resultArray);
+    // resultArray.forEach(element => {
+    //     element = [element[0].split(','), element[1].split(',')];
+    //     resultArray2.push(element);
+    //     // resultArray2.push([element[0].split(','), element[1].split(',')]);
+    // });
+
+    // for (let i = 0; i < resultArray2.length; i++) {
+    //     for (let j = 0; j < resultArray2[i].length; j++) {
+    //         resultArray2[i][j] = parseInt(resultArray2[i][j]);
+    //         // console.log(resultArray2[i][j]);
+    //     }
+    // }
+
+    // console.log('resultArray2:', resultArray2);
+    // return resultArray2;
+    return resultArray;
+}
+
+function createBoard(fileName) {
+    let maxX = 0;
+    let maxY = 0;
+    let xArray = [];
+    let yArray = [];
+    let inputArray = parseInput(fileName);
+    let resultArray = [];
+    for (let i = 0; i < inputArray.length; i++) {
+        let x0 = inputArray[i][0].split(',')[0];
+        let x1 = inputArray[i][1].split(',')[0];
+        xArray.push(parseInt(x0), parseInt(x1));
+        let y0 = inputArray[i][0].split(',')[1];
+        let y1 = inputArray[i][1].split(',')[1];
+        yArray.push(parseInt(y0), parseInt(y1));
+    }
+    xArray.sort();
+    yArray.sort();
+    console.log(xArray);
+    console.log(yArray);
+    maxX = xArray[xArray.length-1];
+    maxY = yArray[yArray.length-1];
+    console.log(maxX, maxY);
+    // added the +1 to maxY and maxX because it seems like it's needed, but haven't fully thought it through
+    for (let y = 0; y < maxY+1; y++) {
+        resultArray[y] = [];
+        for (let x = 0; x < maxX+1; x++) {
+            resultArray[y][x] = 0;
+        }
+    }
+    console.log(resultArray);
+    return resultArray;
+}
+
+function getHorizontalLines(fileName) {
+    const inputArray = parseInput(fileName);
+    let horizontalLineArray = [];
+    console.log(inputArray);
+    for (let i = 0; i < inputArray.length; i++) {
+        console.log(inputArray[i][0]);
+        // if Y is equal
+        if (inputArray[i][0].split(',')[1] === inputArray[i][1].split(',')[1]) {
+            horizontalLineArray.push(inputArray[i]);
+        }
+    }
+    console.log(horizontalLineArray);
+    return horizontalLineArray;
+}
+
+// this is not THAT egregious a DRY violation; I'm tired
+function getVerticalLines(fileName) {
+    const inputArray = parseInput(fileName);
+    let verticalLineArray = [];
+    console.log(inputArray);
+    for (let i = 0; i < inputArray.length; i++) {
+        console.log(inputArray[i][0]);
+        // if X is equal
+        if (inputArray[i][0].split(',')[0] === inputArray[i][1].split(',')[0]) {
+            verticalLineArray.push(inputArray[i]);
+        }
+    }
+    console.log(verticalLineArray);
+    return verticalLineArray;
+}
+
+function plotLines(fileName) {
+    let board = createBoard(fileName);
+    const verticalLines = getVerticalLines(fileName);
+    const horizontalLines = getHorizontalLines(fileName);
+
+    for (let i = 0; i < verticalLines.length; i++) {
+        let x = verticalLines[i][0].split(',')[0];
+        let startY = verticalLines[i][0].split(',')[1];
+        let endY = verticalLines[i][1].split(',')[1];
+        if (startY > endY) {
+            let lowerY = endY;
+            let higherY = startY;
+            startY = lowerY;
+            endY = higherY;
+        }
+        for (let j = startY; j <= endY; j++) {
+            board[j][x]++;
+        }
+    }
+
+    // [
+    //     [ '0,9', '5,9' ],
+    //     [ '9,4', '3,4' ],
+    //     [ '0,9', '2,9' ],
+    //     [ '3,4', '1,4' ]
+    // ]
+    for (let i = 0; i < horizontalLines.length; i++) {
+        let startX = horizontalLines[i][0].split(',')[0];
+        let endX = horizontalLines[i][1].split(',')[0];
+        let y = horizontalLines[i][0].split(',')[1];
+        console.log('y:', y);
+        if (startX > endX) {
+            let lowerX = endX;
+            let higherX = startX;
+            startX = lowerX;
+            endX = higherX;
+        }
+        console.log('startX, endX:', startX, endX);
+        // console.log('board again:', board);
+        for (let j = startX; j <= endX; j++) {
+            console.log(board[y]);
+            board[y][j]++; // this should be the one that works
+            // board[j][y]++;
+        }
+    }
+    console.log(board);
+    return board;
+}
+
+function run(fileName) {
+    // readInput(fileName);
+    // parseInput(fileName);
+    // createBoard(fileName);
+    // getVerticalLines(fileName);
+    // getHorizontalLines(fileName);
+    plotLines(fileName);
+}
+
+run('day5TestInput.txt');
+// run('day5TestInput2.txt');
